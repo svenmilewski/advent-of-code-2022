a583354baeeadfccd3cf6ac52039484b135bedde
Sven Milewski <7903139+svenmilewski@users.noreply.github.com>
2022-12-04 17:03:42 +0100

    day 2

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
-export const getFromMap = <K,V>(map: Map<K, V>, index: K) => {
-    const value = map.get(index);
-    if (typeof value === 'undefined' || value === null) {
-        throw new Error(`Value not found for key: ${index}`);
-    }
-    return value;
-}
+export const getFromMap = <K, V>(map: Map<K, V>, index: K) => {
+  const value = map.get(index);
+  if (typeof value === 'undefined' || value === null) {
+    throw new Error(`Value not found for key: ${index}`);
+  }
+  return value;
+};
